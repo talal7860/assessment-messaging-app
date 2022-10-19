4c0da306156154679c021489e85ce1b70abeda56
Talal Arshad <talal7860@gmail.com>
2022-10-19 11:53:24 +0500

    Build user resource, added seeder

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minmum: 3, maximum: 15}
+    has_secure_password
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "Afzal", password: "secret")
+User.create(username: "Duram", password: "secret")
+User.create(username: "Talal", password: "secret")
+User.create(username: "Hassan", password: "secret")
+User.create(username: "Afraz", password: "secret")
