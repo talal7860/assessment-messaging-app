45f64fdccafedce57ea4983772c8d32ab94d57b1
Talal Arshad <talal7860@gmail.com>
2022-10-19 12:43:55 +0500

    Create Message migration and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minmum: 3, maximum: 15}
     has_secure_password
+
+    has_many :messages
 end
